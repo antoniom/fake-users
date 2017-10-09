44c4df69aa4c13a583d7bdf86a1d6047c3aac5c9
Antonis Balasas <a.balasas@beta-cae.com>
2017-10-09 11:00:47 +0300

    Refactored code to controllers

---
+++ controllers/User.js
@@ -0,0 +1,94 @@
+const User = require('../models/User.js')
+
+function listUsers(req, res) {
+    "use strict";
+    let conditions = {}
+    if (req.query.country) {
+        conditions = {
+            where: {
+                country: req.query.country
+            }
+        }
+    }
+
+    User.findAll(conditions).then(users => {
+        res.set('Content-Type', 'application/json')
+        res.send(users)
+    })
+}
+
+function createUser(req, res) {
+    "use strict";
+    User.build({
+        username: req.body.username,
+        password: req.body.password,
+        displayName: req.body.displayName,
+        birthDate: req.body.birthDate,
+        country: req.body.country
+    })
+        .validate()
+        .then(user => user.save())
+        .then(() => res.sendStatus(201))
+        .catch(error => {
+            let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
+            res.set('Content-Type', 'application/json').status(httpCode).send(error)
+        })
+}
+
+function updateUser(req, res) {
+    "use strict";
+    let username = req.params.username
+
+    User
+        .findById(username)
+        .then((user) => user.update({
+                username: req.body.username,
+                password: req.body.password,
+                displayName: req.body.displayName,
+                birthDate: req.body.birthDate,
+                country: req.body.country
+            }, {})
+        )
+        .then(() => res.sendStatus(200))
+        .catch(error => {
+            let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
+            res.status(httpCode).send(error)
+        })
+}
+
+function deleteUser(req, res) {
+    "use strict";
+    let username = req.params.username
+
+    User
+        .findById(username)
+        .then(user => user.destroy())
+        .then(() => {
+            res.sendStatus(204)
+        })
+        .catch(error => {
+            let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
+            res.status(httpCode).send(error)
+        })
+}
+
+function deleteAllUsers(req, res) {
+    "use strict";
+    User
+        .destroy({where: {}})
+        .then(() => {
+            res.sendStatus(204)
+        })
+        .catch(error => {
+            let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
+            res.status(httpCode).send(error)
+        })
+}
+
+module.exports = {
+    listUsers,
+    createUser,
+    updateUser,
+    deleteUser,
+    deleteAllUsers
+}
--- index.js
+++ index.js
@@ -1,91 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const User = require('./models/User.js')
+const {listUsers, createUser,  updateUser,  deleteUser, deleteAllUsers} = require('./controllers/User.js')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/users', function (req, res) {
-  let conditions = {}
-  if (req.query.country) {
-    conditions = {
-      where: {
-        country: req.query.country
-      }
-    }
-  }
+app.get('/users', listUsers)
 
-  User.findAll(conditions).then(users => {
-    res.set('Content-Type', 'application/json')
-    res.send(users)
-  })
-})
+app.post('/users', createUser)
 
-app.post('/users', function (req, res) {
-  res.set('Content-Type', 'application/json')
+app.put('/users/:username', updateUser)
 
-  User.build({
-    username: req.body.username,
-    password: req.body.password,
-    displayName: req.body.displayName,
-    birthDate: req.body.birthDate,
-    country: req.body.country
-  })
-    .validate()
-    .then(user => user.save())
-    .then(() => res.sendStatus(201))
-    .catch(error => {
-      let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
-      res.status(httpCode).send(error)
-    })
-})
+app.delete('/users/:username', deleteUser)
 
-app.put('/users/:username', function (req, res) {
-  let username = req.params.username
-
-  User
-    .findById(username)
-    .then((user) => user.update({
-      username: req.body.username,
-      password: req.body.password,
-      displayName: req.body.displayName,
-      birthDate: req.body.birthDate,
-      country: req.body.country
-    }, {})
-    )
-    .then(() => res.sendStatus(200))
-    .catch(error => {
-      let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
-      res.status(httpCode).send(error)
-    })
-})
-
-app.delete('/users/:username', function (req, res) {
-  let username = req.params.username
-
-  User
-    .findById(username)
-    .then(user => user.destroy())
-    .then(() => {
-      res.sendStatus(204)
-    })
-    .catch(error => {
-      let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
-      res.status(httpCode).send(error)
-    })
-})
-
-app.delete('/users', function (req, res) {
-  User
-    .destroy({where: {}})
-    .then(() => {
-      res.sendStatus(204)
-    })
-    .catch(error => {
-      let httpCode = error.name === 'SequelizeValidationError' ? 422 : 500
-      res.status(httpCode).send(error)
-    })
-})
+app.delete('/users', deleteAllUsers)
 
 app.listen(8081)
